7990fd0372a33e8af93751c11fa9ac8a2870270b
Achuthananda M P <achuthadivine@gmail.com>
2021-07-11 20:05:29 +0530

    Setup.py Updated

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
 import setuptools
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+import os
+
+
+PACKAGE_NAME = 'akamaiproperty'
+PACKAGE_KEYWORDS = [
+    'Akamai',
+    'Property',
+    'CDN',
+    'AkamaiConfigs',
+    'Edge',
+]
+
+project_root = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the project's README.rst file
+with open(os.path.join(project_root, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
-     name='akamaiproperty',
-     version='2.0',
+     name=PACKAGE_NAME,
+     version='2.1',
      author="Achuthananda M P",
      author_email="achuthadivine@gmail.com",
      description="A Pip Package for Akamai Property",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/Achuthananda/AkamaiProperty",
+     url="https://github.com/Achuthananda/AkamaiPropertyManager",
      packages=['akamaiproperty'],
      install_requires=['edgegrid-python','requests'],
+     keywords=" ".join(PACKAGE_KEYWORDS),
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
